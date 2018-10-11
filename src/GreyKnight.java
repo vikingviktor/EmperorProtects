13c57967b0caf3dd8987d2f288384130ae5a3ad0
Victor Rubio Alonso <victor.rubio.11@gmail.com>
2018-10-11 20:29:57 +0200

    ForThe Emperor

---
+++ src/GreyKnight.java
@@ -0,0 +1,10 @@
+
+public class GreyKnight {
+	
+	public GreyKnight() {
+		
+	}
+	
+	
+
+}
---
+++ src/SpaceMarine.java
@@ -0,0 +1,44 @@
+
+public class SpaceMarine {
+	
+	protected String name;
+	protected String world;
+	protected String squad;
+	private int attack;
+	private int defence;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getWorld() {
+		return world;
+	}
+	public void setWorld(String world) {
+		this.world = world;
+	}
+	public String getSquad() {
+		return squad;
+	}
+	public void setSquad(String squad) {
+		this.squad = squad;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	public int getDefence() {
+		return defence;
+	}
+	public void setDefence(int defence) {
+		this.defence = defence;
+	}
+	
+	
+
+}
